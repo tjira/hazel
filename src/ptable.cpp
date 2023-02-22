2ff026baf946ee1ffc555a7b0917fd02faff6a33
Tomas Jira <tom.jira@gmail.com>
2023-02-22 13:47:18 +0100

    add lithium and beryllium to the periodic table

--- src/ptable.cpp
+++ src/ptable.cpp
@@ -5,6 +5,8 @@
 // https://sciencenotes.org/wp-content/uploads/2019/07/CPK-JmolPeriodicTable.pdf
 std::unordered_map<std::string, Atom> ptable = {
     {  "H", { .radius = 053.0f, .covalent = 032.0f, .color = { 255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f }, .mass = 001.0078 } },
+    { "Li", { .radius = 167.0f, .covalent = 133.0f, .color = { 204.0f / 255.0f, 128.0f / 255.0f, 255.0f / 255.0f }, .mass = 006.9410 } },
+    { "Be", { .radius = 112.0f, .covalent = 102.0f, .color = { 194.0f / 255.0f, 255.0f / 255.0f, 000.0f / 255.0f }, .mass = 009.0122 } },
     {  "C", { .radius = 067.0f, .covalent = 075.0f, .color = { 144.0f / 255.0f, 144.0f / 255.0f, 144.0f / 255.0f }, .mass = 012.0110 } },
     {  "N", { .radius = 056.0f, .covalent = 071.0f, .color = { 048.0f / 255.0f, 080.0f / 255.0f, 248.0f / 255.0f }, .mass = 014.0067 } },
     {  "O", { .radius = 048.0f, .covalent = 063.0f, .color = { 255.0f / 255.0f, 013.0f / 255.0f, 013.0f / 255.0f }, .mass = 015.9994 } },
