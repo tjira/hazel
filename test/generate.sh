e63981c82b83b889f20324192cdef93c06b5498a
Tomas Jira <tom.jira@gmail.com>
2023-07-02 14:04:04 +0200

    add tests

---
+++ test/generate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SYSTEMS=("acetone" "ammonia" "ethanol" "methane" "pyrrole" "water")
+BASES=("sto-3g" "6-31g" "6-311g" "cc-pvdz")
+
+for SYSTEM in ${SYSTEMS[@]}; do
+    for BASIS in ${BASES[@]}; do
+        # echo the source file name
+        FILE="energy_${SYSTEM,,}_hf_${BASIS//-/}.cpp"; echo "$FILE"
+
+        # create the source file
+        cat template/energy.in > "$FILE"
+
+        # calculate the expected energy
+        ENERGY=$(../bin/hazel -b "$BASIS" -f "../example/molecule/$SYSTEM.xyz" hf -d 3 5 -m 1000 -t 1e-8 | grep FINAL | awk '{print $4}')
+
+        # replace values in the source file
+        sed -i "s/BASIS/${BASIS//-/}/g" $FILE && sed -i "s/\"${BASIS//-/}\"/\"${BASIS^^}\"/g" "$FILE"
+        sed -i "s/SYSTEM/$SYSTEM/g" "$FILE" && sed -i "s/EVALUE/$ENERGY/g" "$FILE"
+    done
+done
+
+rm -f gmon.out
