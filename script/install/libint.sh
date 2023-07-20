07fb7341d8ee6b8b7250a05bc2ecfb6d53228a8c
Tomas Jira <tom.jira@gmail.com>
2023-07-20 14:49:18 +0200

    increase angular momentum in libint installation script

--- script/install/libint.sh
+++ script/install/libint.sh
@@ -4,4 +4,4 @@
 git clone --depth 1 https://github.com/evaleev/libint.git libint
 
 # compile libint
-cd libint && ./autogen.sh && ./configure --prefix="$PWD/install" --enable-1body=1 --enable-eri=1 --with-cxxgen-optflags="-O2" && make && make install && cd ..
+cd libint && ./autogen.sh && ./configure --prefix="$PWD/install" --enable-1body=1 --enable-eri=1 --with-max-am=6 --with-cxxgen-optflags="-O2" && make && make install && cd ..
