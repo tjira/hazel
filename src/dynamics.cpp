4dd0a01fa67d1342ad7a054d173f49d0b8a1c8a1
Tomas Jira <tom.jira@gmail.com>
2023-07-25 12:53:25 +0200

    align MD iteration printing

--- src/dynamics.cpp
+++ src/dynamics.cpp
@@ -2,7 +2,7 @@
 
 Dynamics::Results Dynamics::run(System system, const std::function<std::tuple<double, Matrix>(System&)>& egfunc, bool print) const {
     // calculate the initial energy and gradient and print the header with the initial state info
-    auto[E, G] = egfunc(system); if (print) std::printf("ITER        E [Eh]         |GRAD|      TIME\n");
+    auto[E, G] = egfunc(system); if (print) std::printf(" ITER         E [Eh]         |GRAD|      TIME\n");
     if (print) std::printf("%6d %20.14f %.2e %s\n", 0, E, G.norm(), "00:00:00.000");
 
     // create position, velocity and mass matrices
