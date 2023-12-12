ed37db6d7cf8c1c86105eea83a7a554877089f7f
Tomas Jira <tom.jira@gmail.com>
2023-12-12 11:22:48 +0100

    change AVX to default

--- example/generate.sh
+++ example/generate.sh
@@ -476,11 +476,11 @@ cd temp && for MOL in *.xyz; do
     done < "$MOL" && mv "$MOL.tmp" "$MOL" && mv "$MOL" "../molecule/$MOL"
 done && cd .. && rm -r temp
 
-# mkdir -p temp && cp molecule/*.xyz temp && cd temp && for MOL in *.xyz; do
-#     echo -e "! $METHOD $BASIS HCORE MD\n%md timestep $TIMESTEP\ninitvel $TEMP\nthermostat NHC $TEMP\ndump position stride 1 filename \"${MOL%.*}_trj.xyz\"\nrun $STEPS\nend\n*xyzfile 0 1 $MOL" > "${MOL%.*}.inp" && orca "${MOL%.*}.inp" | tee "${MOL%.*}.out"
-#     while read -r LINE; do {
-#         echo "$LINE" | awk 'NF==4 {printf("%2s % 2.10f % 2.10f % 2.10f\n", $1, $2, $3, $4)}';
-#         echo "$LINE" | awk 'NF==13 {printf("%s %i\n", tolower($5), $6)}';
-#         echo "$LINE" | awk 'NF==1 {printf("%s\n", $1)}';} >> "${MOL%.*}_trj.xyz.tmp"
-#     done < "${MOL%.*}_trj.xyz" && mv "${MOL%.*}_trj.xyz.tmp" "${MOL%.*}_trj.xyz" && mv "${MOL%.*}_trj.xyz" "../trajectory/$MOL"
-# done && cd .. && rm -r temp
+mkdir -p temp && cp molecule/*.xyz temp && cd temp && for MOL in *.xyz; do
+    echo -e "! $METHOD $BASIS HCORE MD\n%md timestep $TIMESTEP\ninitvel $TEMP\nthermostat NHC $TEMP\ndump position stride 1 filename \"${MOL%.*}_trj.xyz\"\nrun $STEPS\nend\n*xyzfile 0 1 $MOL" > "${MOL%.*}.inp" && orca "${MOL%.*}.inp" | tee "${MOL%.*}.out"
+    while read -r LINE; do {
+        echo "$LINE" | awk 'NF==4 {printf("%2s % 2.10f % 2.10f % 2.10f\n", $1, $2, $3, $4)}';
+        echo "$LINE" | awk 'NF==13 {printf("%s %i\n", tolower($5), $6)}';
+        echo "$LINE" | awk 'NF==1 {printf("%s\n", $1)}';} >> "${MOL%.*}_trj.xyz.tmp"
+    done < "${MOL%.*}_trj.xyz" && mv "${MOL%.*}_trj.xyz.tmp" "${MOL%.*}_trj.xyz" && mv "${MOL%.*}_trj.xyz" "../trajectory/$MOL"
+done && cd .. && rm -r temp
