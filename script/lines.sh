ad0e20944b942ed047a814fe8a402c27ec6e8637
Tomas Jira <tom.jira@gmail.com>
2024-01-02 11:21:05 +0100

    include tools in line calculation

--- script/lines.sh
+++ script/lines.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat include/* src/* | tr -d "[:blank:]" | sed "/^$/d ; /^\/\//d" | wc -l
+cat include/* src/* tool/hview/include/* tool/hview/src/* tool/hplot/include/* tool/hplot/src/* | tr -d "[:blank:]" | sed "/^$/d ; /^\/\//d" | wc -l
